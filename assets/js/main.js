c71367854fda6fc7e2352d61ce1d2c0d7d52f162
Ahmed Khaled <ahmedaka@icloud.com>
2018-02-17 16:30:06 +0200

    Enhance Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
 
 document.querySelector(".docs-nav__section").addEventListener("click", function(e) {
-
+    
     var elm = e.target.parentNode;
     elm.classList.add("collapse");
     console.log(elm);
 
 });
+
+// Navigation menu behavior
+document.querySelector(".nav-button").addEventListener("click", function(e) {
+    
+    var target = document.querySelector('nav.main-nav');
+    
+    if (target.style.display === 'none') {
+        target.style.display = "flex";
+    } else {
+        target.style.display = 'none';
+    }
+})
